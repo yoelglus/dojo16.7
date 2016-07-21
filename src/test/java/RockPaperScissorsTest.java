ea2169c6146528aed2423b4c6e6fa028d56f95b5
Yoel Gluschnaider <yoelglus@gmail.com>
2016-07-22 00:48:14 +0100

    Make tests have the same domain language as the specs

--- src/test/java/RockPaperScissorsTest.java
+++ src/test/java/RockPaperScissorsTest.java
@@ -24,131 +24,67 @@ public class RockPaperScissorsTest {
     }
 
     @Test
-    public void rockBeatsRockReturnsNull() throws Exception {
-        assertBeatsItselfReturnNull(rock);
+    public void draw_if_two_of_the_same_sign() throws Exception {
+        assertDrawIfTwoOf(rock, paper, scissors, lizard, spock);
     }
 
     @Test
-    public void rockBeatsScissors() throws Exception {
-        assertThat(rock.beats(scissors), is(true));
+    public void scissors_cuts_paper() throws Exception {
+        assertBeats(scissors, paper);
     }
 
     @Test
-    public void rockLosesToPaper() throws Exception {
-        assertThat(rock.beats(paper), is(false));
+    public void paper_covers_rock() throws Exception {
+        assertBeats(paper, rock);
     }
 
     @Test
-    public void rockLosesToSpock() throws Exception {
-        assertThat(rock.beats(spock), is(false));
+    public void rock_crashes_lizard() throws Exception {
+        assertBeats(rock, lizard);
     }
 
     @Test
-    public void rockBeatsLizard() throws Exception {
-        assertThat(rock.beats(lizard), is(true));
+    public void lizard_poisons_spock() throws Exception {
+        assertBeats(lizard, spock);
     }
 
     @Test
-    public void paperBeatsPaperReturnsNull() throws Exception {
-        assertBeatsItselfReturnNull(paper);
+    public void spock_smashes_scissors() throws Exception {
+        assertBeats(spock, scissors);
     }
 
     @Test
-    public void paperBeatsRock() throws Exception {
-        assertThat(paper.beats(rock), is(true));
+    public void scissors_decapitates_lizard() throws Exception {
+        assertBeats(scissors, lizard);
     }
 
     @Test
-    public void paperLosesToScissors() throws Exception {
-        assertThat(paper.beats(scissors), is(false));
+    public void lizard_eats_paper() throws Exception {
+        assertBeats(lizard, paper);
     }
 
     @Test
-    public void paperBeatsSpock() throws Exception {
-        assertThat(paper.beats(spock), is(true));
+    public void paper_disproves_spock() throws Exception {
+        assertBeats(paper, spock);
     }
 
     @Test
-    public void paperLosesToLizard() throws Exception {
-        assertThat(paper.beats(lizard), is(false));
+    public void spock_vaporizes_rock() throws Exception {
+        assertBeats(spock, rock);
     }
 
     @Test
-    public void scissorsBeatsScissorsReturnsNull() throws Exception {
-        assertBeatsItselfReturnNull(scissors);
+    public void rock_blunts_scissors() throws Exception {
+        assertBeats(rock, scissors);
     }
 
-    @Test
-    public void scissorsBeatsPaper() throws Exception {
-        assertThat(scissors.beats(paper), is(true));
+    private static void assertBeats(Thing first, Thing second) {
+        assertThat(first.beats(second), is(true));
     }
 
-    @Test
-    public void scissorsLosesToRock() throws Exception {
-        assertThat(scissors.beats(rock), is(false));
-    }
-
-    @Test
-    public void scissorsLosesToSpock() throws Exception {
-        assertThat(scissors.beats(spock), is(false));
-    }
-
-    @Test
-    public void scissorsBeatsLizard() throws Exception {
-        assertThat(scissors.beats(lizard), is(true));
-    }
-
-    @Test
-    public void spockVsSpockReturnNull() throws Exception {
-        assertBeatsItselfReturnNull(spock);
-    }
-
-    @Test
-    public void spockBeatsScissors() throws Exception {
-        assertThat(spock.beats(scissors), is(true));
-    }
-
-    @Test
-    public void spockBeatsRock() throws Exception {
-        assertThat(spock.beats(rock), is(true));
-    }
-
-    @Test
-    public void spockLosesToPaper() throws Exception {
-        assertThat(spock.beats(paper), is(false));
-    }
-
-    @Test
-    public void spockLosesToLizard() throws Exception {
-        assertThat(spock.beats(lizard), is(false));
-    }
-
-    @Test
-    public void lizardVsLizardReturnNull() throws Exception {
-        assertBeatsItselfReturnNull(lizard);
-    }
-
-    @Test
-    public void lizardLosesToRock() throws Exception {
-        assertThat(lizard.beats(rock), is(false));
-    }
-
-    @Test
-    public void lizardLosesToScissors() throws Exception {
-        assertThat(lizard.beats(scissors), is(false));
-    }
-
-    @Test
-    public void lizardBeatsPaper() throws Exception {
-        assertThat(lizard.beats(paper), is(true));
-    }
-
-    @Test
-    public void lizardBeatsSpock() throws Exception {
-        assertThat(lizard.beats(spock), is(true));
-    }
-
-    private void assertBeatsItselfReturnNull(Thing thing) {
-        assertThat(thing.beats(thing), is(nullValue()));
+    private static void assertDrawIfTwoOf(Thing... things) {
+        for (Thing thing: things) {
+            assertThat(thing.beats(thing), is(nullValue()));
+        }
     }
 }
